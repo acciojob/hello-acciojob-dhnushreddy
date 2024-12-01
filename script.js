509c2e53dbb302fbdd5c703199622a376e2fb33f
Dhanush <110124614+dhnushreddy@users.noreply.github.com>
2024-12-01 12:56:49 +0530

    created a submission

--- script.js
+++
@@ -1,9 +0,0 @@
-//your code here
-const main = document.createElement('div');
-main.className='accio';
-main.id='job';
-const p = document.createElement('p');
-p.className='hello-accio';
-p.textContent='Hello Accio Job';
-main.appendChild(p);
-document.body.appendChild(main);
